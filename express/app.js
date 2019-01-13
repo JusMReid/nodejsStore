e86398e1d760a58023713c80a920a5e5cd51db8f
Justin Reid <Jusmreid@gmail.com>
2019-01-12 22:03:34 -0500

    Styling & Helper Functions
    Added more styling & included helper functions for navigation.
    Also included functionality to serve pages statically

--- express/app.js
+++ express/app.js
@@ -1,12 +1,12 @@
 //const http = require('http');
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-const adminRoutes = require('./routes/admin.js');
-const shopRoutes = require('./routes/shop.js');
+const adminRoutes = require("./routes/admin.js");
+const shopRoutes = require("./routes/shop.js");
 
 /*
 app.use('/', (res,req,next)=>{
@@ -14,13 +14,15 @@ app.use('/', (res,req,next)=>{
     next();
 }); */
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/admin', adminRoutes);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views', 'pnf.html'));
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "pnf.html"));
 });
 
 /*const server = http.createServer(app);
