9b55d633800258fc6f078f25081519c4ae089236
Justin Reid <Jusmreid@gmail.com>
2019-01-31 17:37:14 -0500

    Implemented handlerbars templates

--- express/app.js
+++ express/app.js
@@ -3,15 +3,19 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require('express-handlebars');
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.engine('hbs', expressHbs());
-app.set('view engine', 'hbs');
+app.engine(
+  "hbs",
+  expressHbs({ layoutsDir: "views/layouts/", defaultLayout: "main-layout",
+extname: 'hbs' })
+);
+app.set("view engine", "hbs");
 
 //app.set('view engine', 'pug');
-app.set('views', 'views');
+app.set("views", "views");
 
 const adminData = require("./routes/admin.js");
 const shopRoutes = require("./routes/shop.js");
@@ -30,7 +34,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('pnf', {docTitle: 'PAGE NOT FOUND'});
+  res.status(404).render("pnf", { docTitle: "PAGE NOT FOUND" });
   // res.status(404).sendFile(path.join(__dirname, "views", "pnf.html"));
 });
 
--- express/routes/shop.js
+++ express/routes/shop.js
@@ -1,21 +1,28 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../utils/path');
+const rootDir = require("../utils/path");
 
-const adminData = require('./admin');
+const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get('/',(req, res, next) => {
-    const products = adminData.products;
-    //render the dynamic template
-    res.render('shop', {prods: products,docTitle:'My Shop', path : '/', hasProducts: products.length > 0});
-    // console.log(adminData.products);
-    // //console.log('In another middleware');
-    // //res.send('<h1>Hello from express</h1>');
-    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+router.get("/", (req, res, next) => {
+  const products = adminData.products;
+  //render the dynamic template
+  res.render("shop", {
+    prods: products,
+    docTitle: "My Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
+  // console.log(adminData.products);
+  // //console.log('In another middleware');
+  // //res.send('<h1>Hello from express</h1>');
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
